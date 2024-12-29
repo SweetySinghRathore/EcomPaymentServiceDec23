88c370c491d1a9b60aa512aea4d6e3127d44ec65
SweetySinghRathore <sweety.myr2017@gmail.com>
2024-12-29 21:10:51 +0530

    Payment Service

---
+++ src/main/java/dev/sandeep/PaymentService/EcomPaymentServiceApplication.java
@@ -0,0 +1,14 @@
+package dev.sandeep.PaymentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class EcomPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomPaymentServiceApplication.class, args);
+	}
+
+}
